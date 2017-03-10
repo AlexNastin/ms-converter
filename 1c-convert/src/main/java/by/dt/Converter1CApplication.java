09377700443444eb56c0f43eb0c23a56496fb3dd
Alex Nastin <alexnastin1992@gmail.com>
2017-03-10 13:11:09 +0300

    upd config

--- 1c-convert/src/main/java/by/dt/Converter1CApplication.java
+++ 1c-convert/src/main/java/by/dt/Converter1CApplication.java
@@ -1,11 +1,16 @@
 package by.dt;
 
+import by.dt.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import({WebConfig.class})
+@ComponentScan("by.dt")
 public class Converter1CApplication {
 
 	public static void main(String[] args) {
