b05c934e70c772caf08903e6fff2e09c2736f673
Alex Nastin <alexnastin1992@gmail.com>
2017-05-18 13:17:53 +0300

    update config RabbitMq

--- boao-promtorg/src/main/java/by/dt/promtorg/config/RabbitMQConfiguration.java
+++ boao-promtorg/src/main/java/by/dt/promtorg/config/RabbitMQConfiguration.java
@@ -1,10 +1,11 @@
 package by.dt.promtorg.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
@@ -25,6 +26,33 @@ public class RabbitMQConfiguration {
 
     @Value("${rabbitmq.exchange.name}")
     private String exchange;
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.user}")
+    private String user;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
+        connectionFactory.setUsername(user);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
 
     @Bean
     public Queue queueGeneral() {
